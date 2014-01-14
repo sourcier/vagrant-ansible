da6d0bb3a4cd13870175c3f94f1c7b31bf295294
Roger Ragulan Rajaratnam <roger@ragusource.com>
2014-01-14 00:55:42 +0000

    Create attributes for php.

--- chef/site-cookbooks/ragusource/recipes/php.rb
+++ chef/site-cookbooks/ragusource/recipes/php.rb
@@ -5,3 +5,16 @@
 package 'php5'
 package 'php5-mysql'
 package 'php5-xdebug'
+
+template "/etc/php5/apache2/php.ini" do
+    action :create
+    source "php.erb"
+    mode 0644
+    variables(:options => node['php']['options'])
+end
+template "/etc/php5/cli/php.ini" do
+    action :create
+    source "php.erb"
+    mode 0644
+    variables(:options => node['php']['options'])
+end
