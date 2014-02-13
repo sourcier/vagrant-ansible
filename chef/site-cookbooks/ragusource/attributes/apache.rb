f981ddc91af74045ed1fcbcfa3f53222f7727c55
Roger Ragulan Rajaratnam <roger@ragusource.com>
2014-02-13 21:07:15 +0000

    Make it possible to use nfs mounts for vhosts.

--- chef/site-cookbooks/ragusource/attributes/apache.rb
+++ chef/site-cookbooks/ragusource/attributes/apache.rb
@@ -1,2 +1,3 @@
+default['apache']['nfs'] = nil
 default['apache']['vhosts'] = nil
 default['apache']['log_root'] = '/vagrant/logs'
--- chef/site-cookbooks/ragusource/recipes/apache.rb
+++ chef/site-cookbooks/ragusource/recipes/apache.rb
@@ -15,6 +15,22 @@ end
 
 unless node['apache']['vhosts'].nil?
     node['apache']['vhosts'].each do |vhost|
+
+        unless node['apache']['nfs'].nil?
+            directory "create-docroot" do
+                path "/var/www/#{vhost['server_name']}"
+                action :create
+                recursive true
+                not_if "test -d /var/www/#{vhost['server_name']}"
+            end
+
+            execute "mount-docroot" do
+                command "mount #{vhost['nfs_mount']} /var/www/#{vhost['server_name']}"
+                action :run
+                not_if "test -d #{vhost['document_root']}"
+            end
+        end
+
         template "/etc/apache2/sites-enabled/#{vhost['server_name']}.conf" do
             source "vhost.erb"
             variables(
